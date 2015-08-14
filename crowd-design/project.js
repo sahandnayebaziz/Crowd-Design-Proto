6952f51abc5c35acaa263731675252ad0d3f5ae9
Sahand Nayebaziz <sahand@sahand.me>
2015-08-14 01:00:29 -0700

    added the ability to save an image of the canvas to a thumbnail in each decision point

--- crowd-design/project.js
+++ crowd-design/project.js
@@ -1,4 +1,7 @@
 if (Meteor.isClient) {
+
+  var canvas;
+
   Template.decisionsList.helpers({
     decisions: function () {
       
@@ -67,6 +70,26 @@ if (Meteor.isClient) {
       Decisions.update(this._id, {$set: {category: selectedCategory}}, function () {
         console.log("updated category");
       });
+    },
+    "click .thumb1": function (event) { // TODO: get rid of the repeated function below, add an html-data att to mark with thumb to save it in
+      canvas.backgroundColor = "#E3E3E3";
+      var url =  canvas.toDataURL({format: 'jpeg', quality: 0.3})
+      canvas.backgroundColor = null;
+      canvas.setBackgroundColor(null, canvas.renderAll.bind(canvas));
+      Decisions.update(this._id, {$set: {thumb1: url}}, function () {
+        event.target.setAttribute("src", url);
+        console.log(event.target);
+      });
+    },
+    "click .thumb2": function (event) {
+      canvas.backgroundColor = "#E3E3E3";
+      var url =  canvas.toDataURL({format: 'jpeg', quality: 0.3})
+      canvas.backgroundColor = null;
+      canvas.setBackgroundColor(null, canvas.renderAll.bind(canvas));
+      Decisions.update(this._id, {$set: {thumb2: url}}, function () {
+        event.target.setAttribute("src", url);
+        console.log(event.target);
+      });
     }
   });
 
@@ -76,10 +99,124 @@ if (Meteor.isClient) {
       return (cat === this.category);
     }
   });
-}
 
-//  $(".decisionBox").click(function () {
-//        console.log("clicked");
-//        console.log($(this).siblings(".decisionDetails"));
-//        $(this).siblings(".decisionDetails").toggle();
-//  })
+  Template.freeformSketch.rendered = function() {
+    if(!this._rendered) {
+      this._rendered = true;
+
+      // MARK: Create Canvas and add hooks
+      canvas = new fabric.Canvas('practiceSketch');
+      canvas.isDrawingMode = true;
+      canvas.freeDrawingBrush.width = 6
+      canvas.freeDrawingBrush.color = "white"
+
+      var canvasStateStack = [];
+      var canvasRedoStack = [];
+
+      function saveState() {
+        console.log("saved state");
+
+        if (canvasStateStack.length == 30) {
+          canvasStateStack.shift();
+        }
+        if (canvasRedoStack.length != 0) {
+          canvasRedoStack = [];
+        }
+        canvasStateStack.push(JSON.stringify(canvas));
+      }
+
+      saveState();
+      var recordingStates = true;
+
+      canvas.on('object:added', function (e) {
+        if (recordingStates) {
+          saveState();
+        }
+      })
+
+      canvas.on('object:modified', function (e) {
+        if (recordingStates) {
+          saveState();
+        }
+      })
+
+      canvas.on('object:removed', function (e) {
+        if (recordingStates) {
+          saveState();
+        }
+      })
+
+      // selecting colors
+      $(".colorSelector").click(function () {
+        $("img.selectedTool").removeClass("selectedTool");
+        $(this).addClass("selectedTool");
+        canvas.freeDrawingBrush.color = this.getAttribute("data-color");
+        disableEraser();
+      });
+
+      // selecting the eraser
+      $(".eraserTool").click(function () {
+        $("img.selectedTool").removeClass("selectedTool");
+        $(this).addClass("selectedTool");
+        enableEraser();
+      })
+
+      function disableEraser() {
+        canvas.off("mouse:down");
+        canvas.isDrawingMode = true;
+      }
+
+      function enableEraser() {
+        canvas.isDrawingMode = false;
+        canvas.on("mouse:down", function (e) {
+          if (canvas.getActiveGroup()) {
+            recordingStates = false;
+            canvas.getActiveGroup().forEachObject(function (a) {
+              canvas.remove(a);
+            });
+            canvas.discardActiveGroup();
+            recordingStates = true;
+          } else {
+            canvas.remove(canvas.getActiveObject());
+          }
+          canvas.renderAll();
+        });
+      }
+
+      $(".undoSelector").click(function () {
+        if (canvasStateStack.length > 1) {
+          recordingStates = false;
+          var currentState = canvasStateStack.pop();
+          canvasRedoStack.push(currentState);
+
+          var stateToReturnTo = canvasStateStack[canvasStateStack.length - 1];
+          canvas.loadFromJSON(stateToReturnTo);
+          canvas.renderAll();
+          recordingStates = true;
+        }
+      });
+
+      $(".redoSelector").click(function () {
+        if (canvasRedoStack.length > 0) {
+          recordingStates = false;
+          var stateToReturnTo = canvasRedoStack.pop();
+          canvasStateStack.push(stateToReturnTo);
+          canvas.loadFromJSON(stateToReturnTo);
+          canvas.renderAll();
+          recordingStates = true;
+        }
+      });
+
+      $(".clearSelector").click(function () {
+        if (canvas.getObjects().length > 0) {
+          canvas.clear().renderAll();
+          saveState();
+        }
+      })
+
+      function showShadow() {
+        $(".decisionShadow").isHidden = false;
+      }
+    }
+  }
+}
